67ecc9027008d5129a21a23f964550ab4638ab81
HamazakiHomare <hmre_29@au.com>
2020-03-25 12:40:07 +0900

    test

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -57,7 +57,7 @@
                         <!-- Authentication Links -->
                         {{-- ログインしていなかったらログイン画面へのリンクを表示 --}}
                         @guest
-                            <li><a class="nav-link" href="/twitter/login"><i class="fab fa-twitter"></i> Twitterで登録 / ログイン</a></li>
+                            <li><a class="nav-link" href="/twitter/login">Twitterで登録 / ログイン</a></li>
                         {{-- ログインしていたらユーザー名とログアウトボタンを表示 --}}
                         @else
                             <li class="nav-item dropdown">
