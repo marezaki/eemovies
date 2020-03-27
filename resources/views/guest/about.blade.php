9f1d15fe3b882c384a30559b4e2a7cf1a5f114f8
HamazakiHomare <hmre_29@au.com>
2020-03-27 17:55:07 +0900

    aboutページの修正

--- resources/views/guest/about.blade.php
+++ resources/views/guest/about.blade.php
@@ -5,8 +5,8 @@
 
 @section('content')
     <div class="top-container">
-        <div class="content-wrapper">
-            <div class="js-trigger left-container">
+        <div class="top-wrapper">
+            <div class="js-trigger top-content">
                 <h3 class="fade-elem comment">みんなの<span>「感情」</span>から</h2>
                 <h3 class="fade-elem comment">あなたの<span>「気分」</span>にあった作品を</h2>
                 <h3 class="fade-elem comment">Emotional Evaluation　<img class="logo-image" src="https://eemovies.s3-ap-northeast-1.amazonaws.com/logo/logo_red.png"></h4>
@@ -16,8 +16,8 @@
 
     <div class="middle-container">
         <div class="middle-wrapper">
-            <div class="middle-left-container">
-                <h2 class="js-trigger fade-type-scale">Emotional Evaluation</h2>
+            <h2 class="js-trigger fade-type-scale">Emotional Evaluation</h2>
+            <div class="middle-left-content">
                 <h3 class="js-trigger fade-type-scale">6つの感情で評価</h3>
                 <p class="js-trigger fade-elem">
                     喜び、怒り、悲しみ、楽しさ、笑い、怖さの6つの感情を
@@ -29,7 +29,7 @@
                     あなたの今の気分にぴったりの作品を探してみましょう！
                 </p>
             </div>
-            <div class="middle-right-container js-trigger fade-type-scale">
+            <div class="middle-right-content js-trigger fade-type-scale">
                 <div class="emotional-box">
                     <div class="emotional">
                         <i class="far fa-smile-beam"><label class="level">喜び度</label></i>
@@ -48,8 +48,8 @@
 
     <div class="bottom-container">
         <div class="bottom-wrapper">
-            <div class="bottom-container">
-                <h2 class="js-trigger fade-type-scale">Give Your Rating</h2>
+            <h2 class="js-trigger fade-type-scale">Give Your Rating</h2>
+            <div class="bottom-content">
                 <p class="js-trigger fade-elem">
                     EE MOVIESでは「E評価」が最高評価です
                     <br>
@@ -64,8 +64,6 @@
                     <img class="evaluation-image" src="https://eemovies.s3-ap-northeast-1.amazonaws.com/evaluation/d-1015532_640.jpg">
                     <img class="evaluation-image" src="https://eemovies.s3-ap-northeast-1.amazonaws.com/evaluation/e-1015533_640.jpg">
                 </div>
-            </div>
-            <div class="bottom-right-container">
             </div>
         </div>
     </div>
